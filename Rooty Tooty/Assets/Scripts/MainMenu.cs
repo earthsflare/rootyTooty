27c3278c8500521e7abf6d2c29dc9bed20ac4bf1
WilliamsNguyen <williamsnguyen25@gmail.com>
2022-08-23 02:45:53 -0700

    Main Menu Creation
    I created a main menu screen with the play, options and quit button.
    Play button leads to the next scene in the queue (the game), Options leads to an options menu where the player can adjust the volume through a slider, and the quit button will quit the application.

---
+++ Rooty Tooty/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // used whenever we want to change scenes in Unity
+
+public class MainMenu : MonoBehaviour
+{
+    // function that is called when the play button is pressed
+    public void PlayGame ()
+    {
+        // load the next level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // loads the next level in the queue
+        // done by getting the currently loaded level and increase it by 1
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+}
+s
