bd7e590e02508a4a051419a386f460cd96f5abbb
Tshulkey <tshulkey@gmail.com>
2022-08-23 23:48:23 -0700

    PlayerMovement added a jump
    player will be able to jump
    also added the sprite and animation for idle.

--- Rooty Tooty/Assets/Scripts/PlayerMovement.cs
+++ Rooty Tooty/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
 
-    //vectors store x and y horizontal and vertical
-    Vector2 movement;
+    //TODO: Flip Character and create colliders for character and other objects.
+    //TODO: For platforms and Floor give them the ground layer so the program knows when the character is on the ground and won't fall through
+
+    public float regSpeed = 5f;
+    public float characterSpeed;                //characterSpeed changes if the player chooses to sprint
+    public float jumpHeight = 8f;
+    public bool isJumping = false;
+    public Transform ceilingCheck;              //make sure the character can't jump past a ceiling
+    public Transform groundCheck;               //make sure the character doesn't fall through the ground
+    public LayerMask groundObjects;             //layer to assign the platforms and ground to ground so that we can check when the player is landed.
+    public float checkRadius;
+    public int MAXJUMPS = 2;                       //Double Jumping or more
+
+    private int jumpCounter;                    //Current amount of jumps
+    private bool isGrounded;
+
+    public Rigidbody2D rb;
+    Vector2 movement;                           //vectors store x and y horizontal and vertical
+
+    private void start()
+    {
+        jumpCounter = 0;
+    }
 
 
     // Update is called once per frame
@@ -16,14 +35,44 @@ public class PlayerMovement : MonoBehaviour
     {
         //gives -1 or 1 depending on input ex. left is -1 right is 1
         movement.x = Input.GetAxisRaw("Horizontal");
-//        movement.y = Input.GetAxisRaw("Vertical");
+        //        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift)){
+            characterSpeed = regSpeed * 2;
+        }
+        else
+        {
+            characterSpeed = regSpeed;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCounter < MAXJUMPS)
+        {
+            isJumping = true;
+        }
 
 
     }
 
+    //Called many times per frame
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * characterSpeed * Time.fixedDeltaTime);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+
+
+        if (isGrounded)
+        {
+            jumpCounter = 0;
+        }
+
+        //Add jump force if the player used the jump key and perform a jump
+        if (isJumping && jumpCounter < MAXJUMPS)
+        {
+            rb.AddForce(new Vector2(0f, jumpHeight));
+            jumpCounter++;
+        }
+        isJumping = false;
 
     }
 }
