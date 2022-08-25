e9ebf36689c6182dd8762f340f84cb2df0f0de82
Tshulkey <tshulkey@gmail.com>
2022-08-24 17:36:08 -0700

    Added the sprite animation
    Transitions should work properly now.

--- Rooty Tooty/Assets/Scripts/PlayerMovement.cs
+++ Rooty Tooty/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // ctrl + shift + / will comment out
 
     //TODO: Flip Character and create colliders for character and other objects.
     //TODO: For platforms and Floor give them the ground layer so the program knows when the character is on the ground and won't fall through
 
     public float regSpeed = 5f;
-    public float characterSpeed;                //characterSpeed changes if the player chooses to sprint
+    public float characterSpeed = 0f;                //characterSpeed changes if the player chooses to sprint
     public float jumpHeight = 8f;
     public bool isJumping = false;
     public Transform ceilingCheck;              //make sure the character can't jump past a ceiling
     public Transform groundCheck;               //make sure the character doesn't fall through the ground
     public LayerMask groundObjects;             //layer to assign the platforms and ground to ground so that we can check when the player is landed.
     public float checkRadius;
-    public int MAXJUMPS = 2;                       //Double Jumping or more
+    public int MAXJUMPS = 2;                    //Double Jumping or more
+    public Animator animator;                   //Link the animator to this script so that it will change with the correct input
 
     private int jumpCounter;                    //Current amount of jumps
     private bool isGrounded;
@@ -34,16 +36,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //gives -1 or 1 depending on input ex. left is -1 right is 1
-        movement.x = Input.GetAxisRaw("Horizontal");
-        //        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal") * characterSpeed;
 
-        if (Input.GetKey(KeyCode.LeftShift)){
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
             characterSpeed = regSpeed * 2;
         }
         else
         {
             characterSpeed = regSpeed;
         }
+
+        animator.SetFloat("Speed", Mathf.Abs(movement.x));
 
         if (Input.GetButtonDown("Jump") && jumpCounter < MAXJUMPS)
         {
@@ -56,7 +60,7 @@ public class PlayerMovement : MonoBehaviour
     //Called many times per frame
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * characterSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
 
