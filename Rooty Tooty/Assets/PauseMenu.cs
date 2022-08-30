ff19a8b890dcbbe48f9279a937e66e39b709f0ca
WilliamsNguyen <williamsnguyen25@gmail.com>
2022-08-30 00:43:25 -0700

    Created the pause menu
    I created the pause menu in one of our sample scenes to add to our game hierarchy later, only fix I need to do is the main menu button isn't working

---
+++ Rooty Tooty/Assets/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //a variable that keeps track of whether or not our game is paused
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // when resuming, exit pause menu, continue time in our game, and change GameIsPaused to false
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    // when pausing, bring up pause menu, freeze time in our game, and change our GameIsPaused to true
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+}
