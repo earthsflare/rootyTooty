e6e6163998b4d426c9ae042860286cd1c014579a
Christopher Suga <chrissuga626@gmail.com>
2022-09-01 20:54:26 -0700

    Fixed Charge

--- Rooty Tooty/Assets/Scripts/EnemyMovement.cs
+++ Rooty Tooty/Assets/Scripts/EnemyMovement.cs
@@ -15,14 +15,21 @@ public class EnemyMovement : MonoBehaviour
     [Tooltip("A bool for determining whether the enemy is facing left")]
     bool facingLeft = true;
     [Tooltip("A bool for determining whether the player is in range")]
-    //Enemy Vision Range is determined by a Circle Collider 2D on the player GameObject
+    //Enemy Vision Range is determined by a Box Collider 2D on the player GameObject
     bool playerInRange = false;
+    [Tooltip("The playable character's main GameObject")]
     public GameObject player;
-    public float inRangeSpeed = 4f;
+    [Tooltip("Th movement speed of the enemy when in charging range of player ")]
+    public float chargeSpeed = 4f;
+    [Tooltip("The Rigidbody2D on the Enemy GameObject")]
     public Rigidbody2D rb;
+    [Tooltip("the desired height for a jump")]
     public float jumpHeight = 1f;
-    float jumpForce;
+    //calculated jump strength needed to reach desired height 
+    private float jumpForce;
+    [Tooltip("A bool that ensures the enemy does his charge animation before chasing the player")]
     bool readyToChase;
+    public float enemyReactionTime = 1;
     void Start()
     {
         //starting frame takes the enemy's starting position and their starting direction
@@ -35,24 +42,16 @@ public class EnemyMovement : MonoBehaviour
     {
         if (playerInRange && readyToChase)
         {
+            FaceTowardsPlayer();
+            CheckSpriteDirection();
             //moves the enemy forward based on speed given and direction
             Debug.Log("moving toward player");
-            Vector2 tempVector2 = Vector2.MoveTowards(transform.position, player.transform.position, inRangeSpeed * Time.deltaTime);
+            Vector2 tempVector2 = Vector2.MoveTowards(transform.position, player.transform.position, chargeSpeed * Time.deltaTime);
             transform.position = new Vector3(tempVector2.x, tempVector2.y, 0);
-            if (transform.position.x > player.transform.position.x)
-            {
-                dir = -1;
-                Debug.Log("Chasing Player Left");
-            }
-            else if (transform.position.x < player.transform.position.x)
-            {
-                dir = 1;
-                Debug.Log("Chasing Player Right");
-            }
-            SpriteDirectionCheck();
         }
         else if (!playerInRange)
         {
+            readyToChase = false;
             //if the enemy is too far to the right from the starting position flips the enemy's direction to the left
             if (transform.position.x > startingPos + dist)
             {
@@ -67,14 +66,14 @@ public class EnemyMovement : MonoBehaviour
                 dir = 1;
                 Debug.Log("Moving Right");
             }
-            SpriteDirectionCheck();
+            CheckSpriteDirection();
             transform.Translate(dir * speed * Time.deltaTime * Vector3.right);
         }
 
         //moves towards player when in range
 
     }
-    //Flips the sprite along the x axis by multiplying the x scale by -1
+    //Flips the sprite along the x axis by multiplying the x scale by -1 Source: https://www.youtube.com/watch?v=Cr-j7EoM8bg
     private void FlipSprite()
     {
         Vector3 currentScale = gameObject.transform.localScale;
@@ -82,7 +81,8 @@ public class EnemyMovement : MonoBehaviour
         gameObject.transform.localScale = currentScale;
         facingLeft = !facingLeft;
     }
-    private void SpriteDirectionCheck()
+    //Function that checks the sprites direction and flips it to the correct orientation
+    private void CheckSpriteDirection()
     {
         //if the enemy is moving to the left and not facing the left calls the flipSprite function
         if (dir == -1 && !facingLeft)
@@ -97,29 +97,54 @@ public class EnemyMovement : MonoBehaviour
             Debug.Log("Flipping Sprite");
         }
     }
+    //Function that checks if the enemy is to the left or to the right of the player. Changes the direction accordingly.
+    private void FaceTowardsPlayer()
+    {
+        //if enemy is to the right of player look left
+        if (transform.position.x > player.transform.position.x)
+        {
+            dir = -1;
+            Debug.Log("Looking At Player (Left)");
+        }
+        //if enemy is to the left of player look right
+        else if (transform.position.x < player.transform.position.x)
+        {
+            dir = 1;
+            Debug.Log("Looking At Player (Right)");
+        }
+    }
+    //Coroutine that is called when first entering aggro range. Turns the enemy sprite to face the player, waits, jumps, waits the same amount, then is ready to begin chasing.
     IEnumerator StartCharge()
     {
         Debug.Log("Coroutine Called");
-        yield return new WaitForSeconds(1);
+        FaceTowardsPlayer();
+        CheckSpriteDirection();
+        yield return new WaitForSeconds(enemyReactionTime);
+        //adds upward force to make enemy jump
         rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(enemyReactionTime);
         readyToChase = true;
     }
+    //When the enemy enters the aggro range the playerInRange bool is set to true and the charge coroutine is started
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        //checks the triggers tag to ensure it is the aggro range
         if (collider.CompareTag("PlayerAggroRange")){
             Debug.Log("Entering Player Range");
             playerInRange = true;
             StartCoroutine(StartCharge());
         }
     }
+    //When the enemy leaves the aggro range the playerInRange bool is set to false, the readyToChase bool is set to false to allow for the charge animation to play again, and the charge coroutine is ended in case it is still running
     private void OnTriggerExit2D(Collider2D collider)
     {
+        //checks the triggers tag to ensure it is the aggro range
         if (collider.CompareTag("PlayerAggroRange"))
         {
             Debug.Log("Exiting Player Range");
             playerInRange = false;
             readyToChase = false;
+            StopCoroutine(StartCharge());
         }
     }
 }
