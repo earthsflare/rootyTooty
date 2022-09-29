55f180439394339c64663db619bb5fad9a22ee09
JustinCheung6 <justincheung6@gmail.com>
2022-09-28 21:01:40 -0700

    - Added email and username to registering playfab (need to implement it fully

--- Rooty Tooty/Assets/Scripts/Playfab/PlayfabManager.cs
+++ Rooty Tooty/Assets/Scripts/Playfab/PlayfabManager.cs
@@ -6,9 +6,41 @@ using PlayFab.ClientModels;
 
 public class PlayfabManager : MonoBehaviour
 {
+    [SerializeField] private string displayNameInput;
+    [SerializeField] private string usernameInput;
+    [SerializeField] private string emailInput;
+    [SerializeField] private string passwordInput;
+
+
     private void Start()
     {
         Login();
+    }
+
+    private void Register()
+    {
+        RegisterPlayFabUserRequest request = new RegisterPlayFabUserRequest
+        {
+            //Display name can be anything
+            DisplayName = displayNameInput,
+            //Username is case sensitive and gets a # ID (like discord) (10 or 100 different combinations)
+            //Different id number = different color for their id 
+            Username = usernameInput,
+            //Have email verification before registering
+            Email = emailInput,
+            //Have checks for safe passwords
+            Password = passwordInput
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, RegisterSuccess, RegisterFail);
+    }
+
+    private void RegisterSuccess(RegisterPlayFabUserResult result)
+    {
+
+    }
+    private void RegisterFail(PlayFabError error)
+    {
+
     }
 
     private void Login()
