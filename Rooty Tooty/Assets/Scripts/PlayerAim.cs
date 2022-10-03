4e107c763f2d94dea44b20747299ae91eb457009
earthsflare <earthsflare@yahoo.com.au>
2022-10-03 01:47:39 -0700

    Make player shoot at mouse properly

--- Rooty Tooty/Assets/Scripts/PlayerAim.cs
+++ Rooty Tooty/Assets/Scripts/PlayerAim.cs
@@ -23,11 +23,11 @@ public class PlayerAim : MonoBehaviour
     {
         // Code for aiming with mouse, right now only shoots in direction player is facing
 
-        // Vector2 mouse = Input.mousePosition;
-        // Vector2 screenPoint = mainCam.WorldToScreenPoint(transform.localPosition);
-        // Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
-        // float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        // transform.rotation = Quaternion.Euler(0f,0f, angle);
+        Vector2 mouse = Input.mousePosition;
+        Vector2 screenPoint = mainCam.WorldToScreenPoint(transform.localPosition);
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        firePoint.rotation = Quaternion.Euler(0f,0f, angle);
 
         if(Input.GetMouseButtonDown(0) && isAvailable)
         {
