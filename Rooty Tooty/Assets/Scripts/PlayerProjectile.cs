eb9cd2ff305321e9156e294fbafb4d5b37d1c49c
earthsflare <earthsflare@yahoo.com.au>
2022-10-03 01:09:09 -0700

    RT-65 Deactivate bullets after time

--- Rooty Tooty/Assets/Scripts/PlayerProjectile.cs
+++ Rooty Tooty/Assets/Scripts/PlayerProjectile.cs
@@ -8,11 +8,23 @@ public class PlayerProjectile : MonoBehaviour
     public int damage = 1; // Placeholder
     public Rigidbody2D projectileRB;
     public GameObject impactAnimation;
+    public float offsetTime = 2f;
+    private float timer = 0f;
 
     // Update is called once per frame
     void Update()
     {
         projectileRB.velocity = transform.right * speed;
+
+        // Deactivate projectile after offsetTime seconds
+        // Not sure how efficient this is?
+        // Destroy(gameObject, offsetTime) might be better, but don't want to destroy
+        timer += Time.deltaTime;
+        if(timer > offsetTime)
+        {
+            timer = 0f;
+            gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D (Collider2D collider)
