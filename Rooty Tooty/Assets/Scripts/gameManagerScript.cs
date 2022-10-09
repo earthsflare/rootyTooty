6df39dcf8f3f4e8771e85b9337f97737a7eb0916
WilliamsNguyen <williamsnguyen25@gmail.com>
2022-10-09 09:29:05 -0700

    Added GO into Sample_1

--- Rooty Tooty/Assets/Scripts/gameManagerScript.cs
+++ Rooty Tooty/Assets/Scripts/gameManagerScript.cs
@@ -2,23 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//namespace RootyTooty
-//{
-    public class gameManagerScript : MonoBehaviour
-    {
-        public static gameManagerScript instance;
 
-        void Awake()
+public class gameManagerScript : MonoBehaviour
+{
+    public static gameManagerScript instance;
+
+    void Awake()
+    {
+        if (instance == null)
         {
-            if (instance == null)
-            {
-                instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else if (instance != this)
-            {
-                Destroy(gameObject);
-            }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
         }
     }
-//}
+}
