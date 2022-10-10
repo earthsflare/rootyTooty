8255a37b4587309b394b5244e87a0fa570a6d6f8
WilliamsNguyen <williamsnguyen25@gmail.com>
2022-10-09 19:26:56 -0700

    Added GO to GameManager

--- Rooty Tooty/Assets/Scripts/gameManagerScript.cs
+++ Rooty Tooty/Assets/Scripts/gameManagerScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gameManagerScript : MonoBehaviour
 {
+    public Text pointsText;
     public static gameManagerScript instance;
 
     void Awake()
@@ -18,5 +20,22 @@ public class gameManagerScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        pointsText.text = score.ToString() + " POINTS";
+
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("SAMPLE_1");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
