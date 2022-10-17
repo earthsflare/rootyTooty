911056aa5890247c46318e7e69e02466d978f84a
Christopher Suga <chrissuga626@gmail.com>
2022-10-17 16:00:30 -0700

    Added Minotaur to Restrospective

--- Rooty Tooty/Assets/Scripts/Enemy Movement/MinotaurMovement.cs
+++ Rooty Tooty/Assets/Scripts/Enemy Movement/MinotaurMovement.cs
@@ -5,29 +5,32 @@ using System;
 
 public class MinotaurMovement : MonoBehaviour
 {
-    [Tooltip("The starting position of the enemy")]
-    private Vector3 startingPos;
+    [Tooltip("The starting position of the charge")]
     private Vector3 chargeStartPos;
+    [Tooltip("The end position of the charge")]
     private Vector3 endPos;
+    [Tooltip("The duration/speed of the charge")]
     public float chargeDuration = 2f;
+    [Tooltip("time variable used in lerp")]
     private float elapsedTime;
+    [Tooltip("random int between 0 - 100 to determine attack")]
     private int attackProbability;
+    [Tooltip("Distance that the minotaur will charge")]
     public float chargeDistance = 10;
+    [Tooltip("Bool to know when enemy is charging")]
     private bool chargeAttack = false;
-    private bool axeAttack = false;
+    [Tooltip("Bool to know when enemy can attack")]
     private bool canAttack = true;
+    [Tooltip("Bool to know when enemy finishes charge")]
     private bool finishedCharge;
+    [Tooltip("float that determines charge attack cooldown")]
     public float ChargeCooldown = 2;
+    [Tooltip("float that determines axe attack cooldown")]
     public float AxeCooldown = 2;
-    public float chargeAnimationSpeed = 1.5f;
-    [Tooltip("The distance the enemy can move Left from the starting position")]
-    public float distL = 5;
-    [Tooltip("The distance the enemy can move Right from the starting position")]
-    public float distR = 5;
-    [Tooltip("The movement speed of the enemy")]
-    public float speed = 2f;
+    [Tooltip("float that edits the animation speed of the minotaur during its charge")]
+    private float chargeAnimationSpeed = 1.5f;
     [Tooltip("The direction the enemy is facing; -1 for left +1 for right")]
-    public int dir;
+    private int dir;
     [Tooltip("A bool for determining whether the enemy is facing left")]
     private bool facingLeft;
     [Tooltip("A bool for determining whether the player is in range")]
@@ -35,50 +38,37 @@ public class MinotaurMovement : MonoBehaviour
     bool inAggroRange = false;
     [Tooltip("The playable character's main GameObject")]
     public GameObject player;
-    [Tooltip("Th movement speed of the enemy when in charging range of player ")]
-    public float chargeSpeed = 4f;
-    [Tooltip("The Rigidbody2D on the Enemy GameObject")]
-    public Rigidbody2D rb;
-    [Tooltip("the desired height for a jump")]
-    public float jumpHeight = 1f;
-    //calculated jump strength needed to reach desired height 
-    private float jumpForce;
-    [Tooltip("A bool that ensures the enemy does his charge animation before chasing the player")]
-    bool readyToChase;
+
+
     public float enemyReactionTime = 1;
-    bool isGrounded = true;
-    bool inMeleeRange = false;
     public Animator enemyAnimator;
     void Start()
     {
         dir = (int)gameObject.transform.localScale.x / Math.Abs((int)gameObject.transform.localScale.x);
         facingLeft = dir == -1;
         //starting frame takes the enemy's starting position and their starting direction
-        startingPos = transform.position;
-        chargeStartPos = transform.position;
-        endPos = new Vector3(transform.position.x + (chargeDistance*dir), transform.position.y, transform.position.z);
+        endPos = new Vector3(transform.position.x + (chargeDistance * dir), transform.position.y, transform.position.z);
         player = GameObject.Find("Player");
-        rb = GetComponent<Rigidbody2D>();
-        //jump calculation from https://gamedevbeginner.com/how-to-jump-in-unity-with-or-without-physics/#jump_unity
-        jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
-
     }
     void Update()
     {
-        if (canAttack) Attack();
-        if (chargeAttack)
+        if (inAggroRange)
         {
-            elapsedTime += Time.deltaTime;
-            float percentageComplete = elapsedTime / chargeDuration;
-            transform.position = Vector3.Lerp(startingPos, endPos, Mathf.SmoothStep(0, 1, percentageComplete));
-        }
-        if ((transform.position.x == startingPos.x - chargeDistance || transform.position.x == startingPos.x + chargeDistance) && !finishedCharge)
-        {
-            enemyAnimator.speed = 1;
-            enemyAnimator.SetBool("isCharging", false);
-            finishedCharge = true;
-            chargeAttack = false;
-            StartCoroutine(ChargeAttackCooldown());
+            if (canAttack) Attack();
+            if (chargeAttack)
+            {
+                elapsedTime += Time.deltaTime;
+                float percentageComplete = elapsedTime / chargeDuration;
+                transform.position = Vector3.Lerp(chargeStartPos, endPos, Mathf.SmoothStep(0, 1, percentageComplete));
+            }
+            if ((transform.position.x == chargeStartPos.x - chargeDistance || transform.position.x == chargeStartPos.x + chargeDistance) && !finishedCharge)
+            {
+                enemyAnimator.speed = 1;
+                enemyAnimator.SetBool("isCharging", false);
+                finishedCharge = true;
+                chargeAttack = false;
+                StartCoroutine(ChargeAttackCooldown());
+            }
         }
     }
     //Flips the sprite along the x axis by multiplying the x scale by -1 Source: https://www.youtube.com/watch?v=Cr-j7EoM8bg
@@ -124,7 +114,7 @@ public class MinotaurMovement : MonoBehaviour
         canAttack = false;
         chargeAttack = true;
         finishedCharge = false;
-        startingPos = transform.position;
+        chargeStartPos = transform.position;
         endPos = new Vector3(transform.position.x + (chargeDistance * dir), transform.position.y, transform.position.z);
         enemyAnimator.speed = chargeAnimationSpeed;
         enemyAnimator.SetBool("isCharging", true);
@@ -160,5 +150,14 @@ public class MinotaurMovement : MonoBehaviour
     {
         yield return new WaitForSeconds(AxeCooldown);
         canAttack = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //checks the triggers tag to ensure it is the aggro range
+        if (collider.CompareTag("PlayerAggroRange"))
+        {
+            Debug.Log("Entering Player Range");
+            inAggroRange = true;
+        }
     }
 }
