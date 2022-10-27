f5544d3da1869eeb23471dfef3c98bb8b8385a44
WilliamsNguyen <48665249+WilliamsNguyen@users.noreply.github.com>
2022-10-26 19:06:27 -0700

    Started fixing the menus + manager
    fixed the pause menu + assigning things into the right place

--- Rooty Tooty/Assets/Scripts/GameManger/gameManagerScript.cs
+++ Rooty Tooty/Assets/Scripts/GameManger/gameManagerScript.cs
@@ -10,23 +10,11 @@ public class gameManagerScript : MonoBehaviour
     //public Text pointsText;
     public static gameManagerScript instance;
 
-    //a variable that keeps track of whether or not our game is paused
-    public static bool GameIsPaused = false;
-
-    public static bool GameIsOver = false;
-
     [SerializeField] private GameObject prefab;
     [Header("Read Only")]
     [SerializeField] private Vector2 spawnPosition;
     //[SerializeField] private bool random;
 
-
-    [Header("Object References")]
-    //gameoverscreen 
-    //public static bool GameIsOver = false;
-    public GameObject goMenuUI;
-
-    public GameObject pauseMenuUI;
 
     void Awake()
     {
@@ -50,72 +38,7 @@ public class gameManagerScript : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (GameIsOver == false)
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                if (GameIsPaused)
-                {
-                    Resume();
-                }
-                else
-                {
-                    Pause();
-                }
-            }
-        }
-        else
-        {
-            Debug.Log("Works");
-        }
-    }
-
-    // when resuming, exit pause menu, continue time in our game, and change GameIsPaused to false
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-    }
-
-    // when pausing, bring up pause menu, freeze time in our game, and change our GameIsPaused to true
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        
-        GameIsPaused = true;
-    }
-
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene("Menu");
-        Resume();
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quitting...");
-        Application.Quit();
-    }
-
-
-    public void Setup()
-    {
-        goMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsOver = true;
-        //  pointsText.text = score.ToString() + " POINTS";
-    }
-
-    public void ExitButton()
-    {
-        //before or after
-        goMenuUI.SetActive(false);
-        SceneManager.LoadScene("Menu");
-    }
+    
 
     public void OnSpawnPlayerPrefab()
     {
