5657796a611e7c42fc52c7e0f0bdee8cc62c9b83
Tshulkey <tshulkey@gmail.com>
2022-11-07 17:55:07 -0800

    Add PlayerWallJump script

---
+++ Rooty Tooty/Assets/Scripts/PlayerWallJump.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallJump : MonoBehaviour
+{
+    bool collideWall = false;
+    int jump = 0;
+    bool canJump = false;
+    bool isSliding;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
