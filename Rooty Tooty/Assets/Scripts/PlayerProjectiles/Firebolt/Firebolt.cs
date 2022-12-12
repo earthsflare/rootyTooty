bed242a6bfe1797476e2ae63f7571968183e8394
earthsflare <earthsflare@yahoo.com.au>
2022-12-12 05:12:45 -0800

    Add projectile sounds

--- Rooty Tooty/Assets/Scripts/PlayerProjectiles/Firebolt/Firebolt.cs
+++ Rooty Tooty/Assets/Scripts/PlayerProjectiles/Firebolt/Firebolt.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Firebolt : PlayerProjectile
 {
+    [SerializeField] private AudioSource impactSound;
     // Coroutine for impact animation
     protected override IEnumerator ImpactAnimation()
     {
         projectileAnimator.Play("Firebolt_Impact");
+        impactSound.Play();
         return base.ImpactAnimation();
     }
 }
--- Rooty Tooty/Assets/Scripts/PlayerProjectiles/Magicbolt.cs
+++ Rooty Tooty/Assets/Scripts/PlayerProjectiles/Magicbolt.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Magicbolt : PlayerProjectile
 {
+    [SerializeField] private AudioSource impactSound;
     // Coroutine for impact animation
     protected override IEnumerator ImpactAnimation()
     {
         projectileAnimator.Play("Player Shoot Impact Animation");
+        impactSound.Play();
         return base.ImpactAnimation();
     }
 }
